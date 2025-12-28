f04a60a726053c71780806b33ca89f4a4179d8b9
Nehal Wandhare <nehalwandhare000@gmail.com>
2025-12-28 17:23:21 +0530

    create database using terraform

---
+++ RDS/rds.tf
@@ -0,0 +1,18 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "admin123"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+
+  vpc_security_group_ids = [
+    aws_security_group.rds_sg.id
+  ]
+
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+
+}
